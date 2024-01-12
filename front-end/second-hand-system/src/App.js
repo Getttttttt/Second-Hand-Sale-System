60992f4d1d731b94fb7294cf301d4ae648c0ad7e
gao-entong <getgao@buaa.edu.cn>
2024-01-12 14:43:36 +0800

    add router and detail front end structure

--- front-end/second-hand-system/src/App.js
+++ front-end/second-hand-system/src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRoutes } from "react-router-dom";
+import routes from "./routes/route";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const element = useRoutes(routes);
+  return <>{element}</>;
 }
-
-export default App;
---
+++ front-end/second-hand-system/src/pages/Home.js
@@ -0,0 +1,11 @@
+
+
+function Home() {
+    return (
+      <div className="App">
+        <p>Hello,I'm Get.</p>
+      </div>
+    );
+  }
+  
+  export default Home;
