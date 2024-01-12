76c1056ab24186af1418bb3a273c699b4a605871
JialuRita <r18510248567@163.com>
2024-01-13 00:02:05 +0800

    test

--- back-end/ApplicationInterface/SecondHandSystemAPIs/src/main/java/com/SecondHandSystem/test/test/TestMerchantDAO.java
+++ back-end/ApplicationInterface/SecondHandSystemAPIs/src/main/java/com/SecondHandSystem/test/test/TestMerchantDAO.java
@@ -5,8 +5,8 @@ import com.SecondHandSystem.factory.DAOFactory;
 
 public class TestMerchantDAO {
     public static void main(String[] args) {
-        System.out.println("start test insert...");
         IMerchantDAO merchantDAOProxy;
+        /*System.out.println("start test insert...");
         try {
             merchantDAOProxy = DAOFactory.getIMerchantDAOInstance();
             merchantDAOProxy.insertMerchant("21377223", "GET", "get21377223", "xxx");
@@ -14,6 +14,25 @@ public class TestMerchantDAO {
         } catch (Exception ex) {
             ex.printStackTrace();
             System.out.println("insert error");
+        }*/
+        /*System.out.println("start test delete...");
+        try {
+            IMerchantDAO merchantDAOProxy;
+            merchantDAOProxy = DAOFactory.getIMerchantDAOInstance();
+            merchantDAOProxy.deleteMerchant("21377223");
+            System.out.println("delete success");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("delete error");
+        }*/
+        System.out.println("start test update...");
+        try {
+            merchantDAOProxy = DAOFactory.getIMerchantDAOInstance();
+            merchantDAOProxy.updateMerchant("21377223", "GET", "21377223get", "A",20,3,"xxx");
+            System.out.println("update success");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("update error");
         }
     }
 }
