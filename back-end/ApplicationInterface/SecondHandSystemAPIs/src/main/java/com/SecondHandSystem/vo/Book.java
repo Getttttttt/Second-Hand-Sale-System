b2db70ffb33305ee32fdfaf7451270449e143f7e
21377063 <jyh21377063@163.com>
2024-01-12 14:48:37 +0800

    add vo part class

---
+++ back-end/ApplicationInterface/SecondHandSystemAPIs/src/main/java/com/SecondHandSystem/vo/Book.java
@@ -0,0 +1,133 @@
+package com.SecondHandSystem.vo;
+
+import java.util.Date;
+
+public class Book {
+    private String bookID;
+    private String bookName;
+    private double bookPrice;
+    private double discount;
+    private int bookNum;
+    private String author;
+    private String bookISBN;
+    private String bookPublisher;
+
+    public String getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(String bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public double getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(double bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public int getBookNum() {
+        return bookNum;
+    }
+
+    public void setBookNum(int bookNum) {
+        this.bookNum = bookNum;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getBookISBN() {
+        return bookISBN;
+    }
+
+    public void setBookISBN(String bookISBN) {
+        this.bookISBN = bookISBN;
+    }
+
+    public String getBookPublisher() {
+        return bookPublisher;
+    }
+
+    public void setBookPublisher(String bookPublisher) {
+        this.bookPublisher = bookPublisher;
+    }
+
+    public String getBookSurfacePic() {
+        return bookSurfacePic;
+    }
+
+    public void setBookSurfacePic(String bookSurfacePic) {
+        this.bookSurfacePic = bookSurfacePic;
+    }
+
+    public String[] getBookRealPics() {
+        return bookRealPics;
+    }
+
+    public void setBookRealPics(String[] bookRealPics) {
+        this.bookRealPics = bookRealPics;
+    }
+
+    public String[] getBookLabels() {
+        return bookLabels;
+    }
+
+    public void setBookLabels(String[] bookLabels) {
+        this.bookLabels = bookLabels;
+    }
+
+    public Date getPublicationTime() {
+        return publicationTime;
+    }
+
+    public void setPublicationTime(Date publicationTime) {
+        this.publicationTime = publicationTime;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public Date getShelfTime() {
+        return shelfTime;
+    }
+
+    public void setShelfTime(Date shelfTime) {
+        this.shelfTime = shelfTime;
+    }
+
+    private String bookSurfacePic;
+    private String[] bookRealPics;
+    private String[] bookLabels;
+    private Date publicationTime;
+    private String degree;//degree of new/old
+    private Date shelfTime;
+}
---
+++ back-end/ApplicationInterface/SecondHandSystemAPIs/src/main/java/com/SecondHandSystem/vo/Order.java
@@ -0,0 +1,78 @@
+package com.SecondHandSystem.vo;
+
+import java.util.Date;
+
+public class Order {
+    private String customerID;
+    private String bookID;
+    private Date tradingTime;
+    private double tradingPrice;
+    private int tradingNum;
+    private String tradingStatus;
+    private int estimationScale;
+    private String evaluation;
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(String bookID) {
+        this.bookID = bookID;
+    }
+
+    public Date getTradingTime() {
+        return tradingTime;
+    }
+
+    public void setTradingTime(Date tradingTime) {
+        this.tradingTime = tradingTime;
+    }
+
+    public double getTradingPrice() {
+        return tradingPrice;
+    }
+
+    public void setTradingPrice(double tradingPrice) {
+        this.tradingPrice = tradingPrice;
+    }
+
+    public int getTradingNum() {
+        return tradingNum;
+    }
+
+    public void setTradingNum(int tradingNum) {
+        this.tradingNum = tradingNum;
+    }
+
+    public String getTradingStatus() {
+        return tradingStatus;
+    }
+
+    public void setTradingStatus(String tradingStatus) {
+        this.tradingStatus = tradingStatus;
+    }
+
+    public int getEstimationScale() {
+        return estimationScale;
+    }
+
+    public void setEstimationScale(int estimationScale) {
+        this.estimationScale = estimationScale;
+    }
+
+    public String getEvaluation() {
+        return evaluation;
+    }
+
+    public void setEvaluation(String evaluation) {
+        this.evaluation = evaluation;
+    }
+}
