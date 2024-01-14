f7c840b7f1d87404781e8035b31678abef453dc7
21377063 <jyh21377063@163.com>
2024-01-14 09:25:00 +0800

    add frame

---
+++ back-end/ApplicationInterface/SecondHandSystemAPIs/src/main/java/com/SecondHandSystem/SecondHandSystemApplication.java
@@ -0,0 +1,11 @@
+package com.SecondHandSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandSystemApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SecondHandSystemApplication.class,args);
+    }
+}
