1c346a647115582e76f13398471a005dacc33bd7
JialuRita <r18510248567@163.com>
2024-01-16 09:59:03 +0800

    front-end: communication

--- front-end/second-hand-system/src/pages/CustomerPages/ChatPages/ChatToSingleMerchant.js
+++ front-end/second-hand-system/src/pages/CustomerPages/ChatPages/ChatToSingleMerchant.js
@@ -1,9 +1,229 @@
 import * as React from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Container } from "@mui/material";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import { CustomerServiceFilled, UserOutlined } from '@ant-design/icons';
+import SendIcon from "@mui/icons-material/Send.js";
+import { Avatar, Badge, Space, Popover } from 'antd';
 
-export default function ChatToSingleMerchant() {
+function Copyright(props) {
   return (
-  <Container>
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-  </Container>);
+
+function ChatCanvas(merchantId, nicknameM, customerId, nicknameC, message, fromWho,image){
+  /*const image = "/node_modules/jsdom/lib/jsdom/browser/resources/image/img.jpg"
+  const id = "ID"
+  const nickname = "nickname"
+  const message = "hello!"
+  const fromWho = "customer"*/
+  
+  const boxcontentleftStle = {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    height: '100vh',
+    marginLeft: '160px',
+  }
+  const boxcontentrightStle = {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    height: '100vh',
+    marginRight: '160px',
+  }
+
+  if(fromWho==="customer"){  
+    return(
+      <Container sx={{justifyContent: 'flex-end'}}>
+        <Box 
+          style={boxcontentrightStle}
+          sx={{
+            marginTop: 13,
+            display: 'flex',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <space>
+            <Popover content={merchantId} title={nicknameM}>
+              <Button type="primary">{message}</Button>
+            </Popover>
+            <d>  </d>
+            <Avatar alt={merchantId+"'s photo"} src={image} />
+
+          </space>
+        </Box>
+      </Container>
+  )
+  }
+  if(fromWho==="merchant"){
+    return(
+      <Container>
+        <Box 
+          style={boxcontentleftStle}
+          sx={{
+            marginTop: 13,
+            display: 'flex',
+            justifyContent: 'flex-start',
+          }}
+        >
+          <space>
+            <Avatar alt={merchantId+"'s photo"} src={image} />
+            <d>  </d>
+            <Popover content={merchantId} title={nicknameM}>
+              <Button type="primary">{message}</Button>
+            </Popover>
+
+          </space>
+        </Box>
+      </Container>
+    )
+  }
+}
+
+
+
+
+export default function ChatToSingleMerchant({ communication },image) {
+  
+  var merchantId
+  var nicknameM
+  var customerId
+  var nicknameC
+  var message
+  var fromWho
+
+  communication.map((item, index) => (
+    merchantId = item[0],
+    nicknameM = item[1],
+    customerId = item[2],
+    nicknameC = item[3],
+    message = item[4],
+    fromWho = item[5] 
+  ))
+  const [inputValue, setInputValue] = useState('');
+  const [chatHistory, setChatHistory] = useState([]);
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  /*const [messages, setMessages] = useState([]);
+  const [inputValue,setInputValue] = useState('');
+  const messagesEndRef = useRef(null);
+
+  // 处理发送消息
+  const sendMessage = () => {
+    if (inputValue.trim() !== '') {
+      const newMessage = {
+        content: inputValue,
+        sender: 'user', // 假设用户发送的消息
+        timestamp: new Date().getTime() // 当前时间戳
+      };
+      setMessages([...messages, inputValue]);
+      setInputValue('');
+    }
+  };*/
+
+  const boxsendStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    height: '100vh'
+  };
+  const inputStyle = {
+    padding: '10px',
+    fontSize: '16px',
+    border: '2px solid #ccc',
+    borderRadius: '5px',
+    outline: 'none',
+    width:'500px'
+  };
+  const boxheadStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    height: '100vh'
+  }
+
+  const sendMessage = () => {
+    ChatCanvas(merchantId, nicknameM, customerId, nicknameC, message, fromWho, image = {image} );
+    setInputValue('');
+    setChatHistory(...chatHistory,inputValue)
+  }
+  
+  return (
+    <container className="chat-container">
+      <container className="header" style={{position: 'fixed', top: 0, left: 0, right: 0, backgroundColor: 'lightgray' }}>
+        <Box
+        style={{backgroundColor:"lightblue"}}
+        sx={{
+          display:'flex',
+          justifyContent: 'center',
+          alignItems: 'flex-start',
+          height: '100vh',
+          marginTop: 2,
+          width: 1500,
+          height: 50,
+          display: 'flex',
+          position: 'absolute',
+          top: 2,
+        }}>
+        <p class="name" style={{ fontSize: '20px', fontWeight: "bold" }}> {nicknameM}</p>
+        </Box>
+      </container>
+      <ChatCanvas 
+        merchantId={merchantId}
+        nicknameM = {nicknameM}
+        customerId = {customerId}
+        nicknameC = {nicknameC}
+        message = {message}
+        fromWho = {fromWho}
+        image = {image} 
+      />
+    <container className="chat-input" style={{position: 'fixed', bottom: 0, left: 0, right: 0}}>
+      <Box
+      style={boxsendStyle}
+      sx={{
+        width: 1500,
+        height: 100,
+        display: 'flex',
+        position: 'absolute',
+        bottom: 2,
+      }}>
+        <Space>
+          <input
+            style={inputStyle}
+            type="text"
+            value={inputValue}
+            onChange={handleChange}
+            placeholder="Type your message..."
+          />
+          <d>  </d>
+          <Button 
+            onClick={sendMessage} 
+            endIcon={<SendIcon />}
+            variant="contained"
+            size="small"
+            sx={{ mt: 5, mb: 5 }}>Send</Button>
+        </Space>
+      </Box>
+    </container>
+  </container>
+  );
 }
--- front-end/second-hand-system/src/pages/CustomerPages/ChatPages/CustomerChatToMerchantList.js
+++ front-end/second-hand-system/src/pages/CustomerPages/ChatPages/CustomerChatToMerchantList.js
@@ -1,9 +1,133 @@
 import * as React from 'react';
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
+import Divider from '@mui/material/Divider';
+import Link from '@mui/material/Link';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Typography from '@mui/material/Typography';
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar, Badge, Space } from 'antd';
+import ChatToSingleCustomer from '../../MerchantPages/ChatPages/ChatToSingleCustomer';
 
-export default function CustomerChatToMerchantList() {
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default function CustomerChatToMerchantList(customerId,{ allMerchants }) {
+  allMerchants={1:"21377225",2:"21377006"}
+  customerId = "21377225"
+
+  const image = "xxx"  //查询商家
+  const nickname =  "abc"
+  
+  return (
+    <Container>
+      <Page />
+
+    </Container>
+  )
+}
+
+
+
+function Page(){
+  const image = "/node_modules/jsdom/lib/jsdom/browser/resources/image/img.jpg"
+  const id = "merchantID"
+  const nickname = "nickname"
+  const lastMessage = "hello"  //查询
+  const time = "2024-1-1 10:00:00"
+  const communication = [
+    ["21377223", 'get', "21377225", 'rita', 'Hello!', "customer"],
+    ["21377223", 'get', "21377225", 'rita', 'Hi,what can I do for you?', "merchant"],
+    ["21377223", 'get', "21377225", 'rita', 'I want to know more about the book.', "customer"],
+  ]
+
   return (
   <Container>
+     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <ListItem alignItems="flex-start">
+        <a href='/customer/chat/merchant/' className="no-underline">
+          <ChatToSingleCustomer 
+            communication={communication}
+            image={image}  
+          />
+        <ListItemAvatar>
+          <Space size={24}>
+            <Badge dot>
+              <Avatar alt={id+"'s photo"} src={image} />
+            </Badge>
+          </Space>
+        </ListItemAvatar>
+        <ListItemText
+          primary={nickname}
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                {id}
+              </Typography>
+              {" — "+lastMessage+"    "+time}
+            </React.Fragment>
+          }
+        />
+        </a>
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Space size={24}>
+            <Badge dot>
+              <Avatar alt={id+"'s photo"} src={image} />
+            </Badge>
+          </Space>
+        </ListItemAvatar>
+        <ListItemText
+          primary={nickname}
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                {id}
+              </Typography>
+              {" — "+lastMessage+"    "+time}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      </List>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          position: 'absolute',
+          bottom: 2,
+          justifyContent: 'center',
+        }}
+      >
+        <Copyright sx={{ mt: 5 }} />
+      </Box>
+      
+  </Container>
+  );
 
-  </Container>);
 }
